2284fe944068e2371930940d5aa21b5c34306c8b
Richard Baptist <richard@brightin.nl>
2015-09-03 20:41:17 +0200

    Merge.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,10 @@ class Category < ActiveRecord::Base
   has_many :categorizations
   has_many :venues, through: :categorizations
 
-  def with_best_venues
+  def self.with_best_venues
     categories = includes(:venues).order('venues.rating DESC').uniq
     cats_and_venues = {}
-    categories.each { |cat| cats_and_venues[cat] = cat.venues.first }
+    categories.each { |cat| cats_and_venues[cat.name] = cat.venues.first.place_id }
     cats_and_venues
   end
 end
