3075323ec8f4ba89f21c55a5f1542f91783ec25a
Richard Baptist <richard@brightin.nl>
2015-09-03 15:47:28 +0200

    Add models for venues and parsing Google Places API results.

--- app/controllers/places.rb
+++ app/controllers/places.rb
@@ -1,9 +1,16 @@
 get '/places' do
-  @results = PlacesApiClient.new.find_places(
-    '41.389261',
-    '2.136956',
-    1000,
-    'food'
+  api_client = PlacesApiClient.new
+  results = api_client.find_places(
+    '41.387063,2.170655',
+    1000
   )
-  @results.to_s
+  venues = results.map do |result|
+    place_id = result[:place_id]
+    details = api_client.place_details(place_id)
+    venue = Venue.new(details)
+    # venue.category = GoogleScraper.new.category(venue.google_url)
+    # venue
+  end
+  binding.pry
+  venues.to_s
 end
--- app/models/places_api_client.rb
+++ app/models/places_api_client.rb
@@ -1,16 +1,31 @@
 class PlacesApiClient
-  BASE_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+  BASE_URL = 'https://maps.googleapis.com/maps/api/place'
   TYPES = %w(food bar cafe restaurant)
 
-  def find_places(lat, lon, radius, types)
-    url = "#{BASE_URL}location=#{lat},#{lon}&radius=#{radius}&types=#{types || default_types}&key=#{ENV['GOOGLE_PLACES_API_KEY']}"
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    # JSON.parse(response)
+  def find_places(coords, radius, types = default_types)
+    params = "location=#{coords}&radius=#{radius}&types=#{types || default_types}&key=#{api_key}"
+    uri = URI("#{BASE_URL}/nearbysearch/json?#{params}")
+    results_for_uri(uri)[:results]
   end
 
-  def default_typess
+  def place_details(place_id)
+    params = "placeid=#{place_id}&key=#{api_key}"
+    uri = URI("#{BASE_URL}/details/json?#{params}")
+    results_for_uri(uri)[:result]
+  end
+
+  private
+
+  def results_for_uri(uri)
+    response = Net::HTTP.get(uri)
+    JSON.parse(response, symbolize_names: true)
+  end
+
+  def default_types
     TYPES.join('|')
   end
 
+  def api_key
+    ENV['GOOGLE_PLACES_API_KEY']
+  end
 end
