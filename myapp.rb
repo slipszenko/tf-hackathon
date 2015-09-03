94801d6d7f8dbe8349aa00dde4e562f6a48d20b3
Edward Slipszenko <edward.slipszenko@myport.ac.uk>
2015-09-03 11:06:38 +0200

    Quick test with Twilio

--- myapp.rb
+++ myapp.rb
@@ -5,10 +5,27 @@ require 'bundler/setup'
 require 'sinatra'
 require 'dotenv'
 require 'typeform'
+require 'twilio-ruby'
 
 Dotenv.load
+
+Twilio.configure do |config|
+  config.account_sid = ENV['TWILIO_SID']
+  config.auth_token = ENV['TWILIO_AUTH_TOKEN']
+end
 
 get '/' do
     puts ENV['TYPEFORM_API_KEY']
     'Hello world!'
+end
+
+get '/twilio_test' do
+    @client = Twilio::REST::Client.new
+
+    @client.messages.create(
+        from: '+34911061281',
+        to: ENV['TEST_PHONE_NUMBER'],
+        body: 'This is a test message!'
+    )
+    'Should have sent the message'
 end
